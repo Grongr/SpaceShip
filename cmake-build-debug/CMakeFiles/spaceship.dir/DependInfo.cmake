affab8c5e468552c5f3962c56abcff6df02ac78c
Egor Osipov <e.a.osipov@mail.ru>
2021-02-13 18:41:18 +0300

    Commit all the files of the project

---
+++ cmake-build-debug/CMakeFiles/spaceship.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/grongr/learning/mipt/informatics/2sem/11.02.2021/spaceship/main.cpp" "/home/grongr/learning/mipt/informatics/2sem/11.02.2021/spaceship/cmake-build-debug/CMakeFiles/spaceship.dir/main.cpp.o"
+  "/home/grongr/learning/mipt/informatics/2sem/11.02.2021/spaceship/spaceship.cpp" "/home/grongr/learning/mipt/informatics/2sem/11.02.2021/spaceship/cmake-build-debug/CMakeFiles/spaceship.dir/spaceship.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/spaceship.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spaceship.dir/main.cpp.o"
+  "CMakeFiles/spaceship.dir/spaceship.cpp.o"
+  "spaceship"
+  "spaceship.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spaceship.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
